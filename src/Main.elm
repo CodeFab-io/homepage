086f893b63c3e64a950979bf5586d2202d61f5b3
Fábio Beirão <fdbeirao@gmail.com>
2023-04-19 22:24:41 +0200

    📝 Finally adding some content
    Content is the hardest part of this whole thing 😅

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ import Assets
 import Browser exposing (Document)
 import Browser.Events
 import Colors
-import Element exposing (Element, alignRight, alignTop, centerX, column, el, fill, fillPortion, height, inFront, link, maximum, minimum, newTabLink, none, padding, paddingXY, paragraph, pointer, px, row, spacing, text, width)
+import Element exposing (Element, alignRight, alignTop, centerX, column, el, fill, fillPortion, height, inFront, link, maximum, minimum, newTabLink, none, padding, paddingEach, paddingXY, paragraph, pointer, px, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Events as Events
@@ -25,6 +25,14 @@ type alias Model =
 type Msg
     = OnResize Int Int
     | ToggleTheme
+
+
+type alias TextUtils msg =
+    { scaled : Int -> Int
+    , normal : String -> Element msg
+    , semiBold : String -> Element msg
+    , italic : String -> Element msg
+    }
 
 
 init : Json.Decode.Value -> ( Model, Cmd Msg )
@@ -105,14 +113,17 @@ phoneView _ =
         scaled =
             Element.modular 16 1.25 >> round
 
-        p =
-            paragraph [ Font.size (scaled 2) ]
+        textUtils =
+            { scaled = scaled
+            , semiBold = el [ Font.size (scaled 1), Font.semiBold ] << text
+            , italic = el [ Font.size (scaled 1), Font.italic ] << text
+            , normal = el [ Font.size (scaled 1) ] << text
+            }
     in
     column
         [ width fill, height fill, spacing (scaled 1), padding (scaled 1) ]
         [ el [ Font.size (scaled 6) ] <| text "codefab.io"
-        , p firstParagraph
-        , p secondParagraph
+        , column [ spacing (scaled 1) ] <| paragraphs textUtils
         ]
 
 
@@ -133,9 +144,17 @@ desktopView model =
 
         p =
             paragraph [ Font.size (scaled 3) ]
+
+        textUtils =
+            { scaled = scaled
+            , semiBold = el [ Font.size (scaled 2), Font.semiBold ] << text
+            , italic = el [ Font.size (scaled 2), Font.italic ] << text
+            , normal = el [ Font.size (scaled 2) ] << text
+            }
     in
     column
         [ width fill
+        , height fill
         , inFront <|
             el
                 [ width fill, padding (scaled 1) ]
@@ -162,10 +181,8 @@ desktopView model =
             [ centerX, width <| (fillPortion 3 |> maximum mainContentMaxWidth) ]
             [ column
                 [ width <| (fillPortion 3 |> maximum (mainContentMaxWidth - sidebarWidth - modularNormal)), spacing (scaled 1) ]
-                [ column [ width fill, height fill, spacing (scaled 1), paddingXY modularNormal modularNormal ]
-                    [ p firstParagraph
-                    , p secondParagraph
-                    ]
+                [ column [ width fill, height fill, spacing (scaled 1), paddingXY modularNormal modularNormal ] <|
+                    paragraphs textUtils
                 ]
             , column
                 [ width <| (fillPortion 1 |> minimum (sidebarWidth + modularNormal)), alignTop ]
@@ -189,6 +206,7 @@ desktopSidebarView { scaled, sidebarWidth } model =
     column
         [ alignRight
         , spacing (scaled 1)
+        , width (px sidebarWidth)
         , Font.size (scaled 1)
         , inFront <| el [ pointer, alignRight, Events.onClick ToggleTheme, padding (scaled -2), Element.htmlAttribute <| title "I have an idea:\ntoggle the theme color here" ] <| text "💡"
         ]
@@ -201,7 +219,10 @@ desktopSidebarView { scaled, sidebarWidth } model =
             ]
             none
         , column [ centerX, spacing (scaled 1) ]
-            [ el [ centerX, Font.bold, Font.size (scaled 3) ] <| text "Fábio Beirão"
+            [ column [ spacing (scaled -3) ]
+                [ el [ centerX, Font.bold, Font.size (scaled 3) ] <| text "Fábio Beirão"
+                , paragraph [ Font.center, Font.size (scaled -1) ] [ text "Freelancer, full-stack software architect and developer" ]
+                ]
             , column [ centerX, spacing (scaled 1) ]
                 [ row [ centerX, spacing (scaled -2) ]
                     [ iconWithLink { title = "linkedin.com", icon = Assets.linkedinLogo, url = "https://www.linkedin.com/in/fdbeirao/" }
@@ -221,14 +242,67 @@ desktopSidebarView { scaled, sidebarWidth } model =
         ]
 
 
-firstParagraph : List (Element msg)
-firstParagraph =
-    [ text <| String.repeat 50 "first line " ]
+paragraphs : TextUtils msg -> List (Element msg)
+paragraphs t =
+    let
+        p =
+            paragraph [ Font.size (t.scaled 3) ]
+    in
+    [ p
+        [ el [ Font.bold, paddingEach { top = 0, left = 0, right = t.scaled -2, bottom = 0 } ] <| text "Welcome!"
+        , t.normal "My name is Fábio Beirão. I am a software architect and developer at heart. I have had the amazing opportunity to collaborate in this constantly evolving field for over "
+        , t.semiBold "12 years"
+        , t.normal " now."
+        ]
+    , p
+        [ t.normal "I am a"
+        , t.semiBold " freelancer "
+        , t.normal " since May 2022. I am originally from"
+        , t.semiBold " Portugal "
+        , t.normal "and I moved to the Netherlands in December 2015. Have I been able to learn Dutch meanwhile? All I can say is: "
+        , t.italic <| quote "Het spijt me, ik praat geen Nederlands. Het is niet makkelijk. Tot ziens, dank je wel."
+        , t.normal " (so: no, not that much)."
+        ]
+    , p
+        [ t.semiBold "Technology-wise"
+        , t.normal " my background has been in"
+        , t.semiBold " dotnet (c#) "
+        , t.normal " (started with .NET Framework 2.0, back in the day),"
+        , t.semiBold " javascript/typescript "
+        , t.normal "using both vanilla as well as more modern frameworks. "
+        , t.normal "Throughout my career have have always been a "
+        , t.semiBold "devop"
+        , t.normal ", even before that was a word. I don't believe in building software that I don't follow-through to production. "
+        , t.normal "It's the best way to learn, I might even say it's the only way."
+        ]
+    , p
+        [ t.normal "This devops experience has made me change the way I approach development."
+        , t.semiBold " Reliability "
+        , t.normal "should never be underestimated. After one too many "
+        , t.italic <| quote "undefined is not a function"
+        , t.normal " or the dreaded "
+        , t.italic <| quote "Unhandled Exception: System.NullReferenceException"
+        , t.normal " (many of them caused by me, oops) I have started gravitating towards different languages and paradigms. Despite most of my previous carrer having been"
+        , t.italic <| quote "object oriented"
+        , t.normal " and imperative, in 2017 I had a ground-breaking pivotal moment when I decided to learn "
+        , t.semiBold "Erlang"
+        , t.normal ", "
+        , t.semiBold "Elixir"
+        , t.normal " and "
+        , t.semiBold "Elm"
+        , t.normal ". These technologies have helped me understand and fully embrace "
+        , t.semiBold "functional programming"
+        , t.normal " as a tool to maximize code reliability. Don't get me wrong, it is not a silver bullet, but the trade-offs it brings are more than worth it (in my experience)."
+        ]
+    , p
+        [ t.normal "📝 Write about AWS, Terraform, Orleans (actor model), Event Sourcing, DDD, Type-Driven development"
+        ]
+    ]
 
 
-secondParagraph : List (Element msg)
-secondParagraph =
-    [ text <| String.repeat 500 "second line " ]
+quote : String -> String
+quote t =
+    "“" ++ t ++ "”"
 
 
 
